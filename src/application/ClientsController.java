a7c578de006256235d1bbc95acf06a46b54cb7f2
Bozhidar Ivanov <bojidar.ivanov5@gmail.com>
2018-11-22 22:01:00 +0200

    Redesign of home and anchorpane, added client save to the table

--- src/application/ClientsController.java
+++ src/application/ClientsController.java
@@ -8,9 +8,12 @@ import javafx.fxml.FXML;
 import com.jfoenix.controls.JFXTreeTableView;
 
 import entity.Client;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
 
 public class ClientsController implements Initializable {
 
@@ -22,6 +25,16 @@ public class ClientsController implements Initializable {
 	@FXML TableColumn<Client, Integer> ageColumn;
 	@FXML TableColumn<Client, String> phoneColumn;
 	@FXML TableColumn<Client, String> emailColumn;
+    @FXML
+    private TextField firstName;
+    @FXML
+    private TextField lastName;
+    @FXML
+    private TextField age;
+    @FXML
+    private TextField email;
+    @FXML
+    private TextField phone;
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
@@ -35,5 +48,33 @@ public class ClientsController implements Initializable {
 		Client exampleClient = new Client("Иван", "Петров", 26, "ipetrov@gmail.com", "08745863125");
 		clientsTable.getItems().add(exampleClient);
 	}
+
+    @FXML
+    private void handleAddClient(ActionEvent event) {
+        int clientAge = Integer.parseInt(this.age.getText());
+        if (!validateAge(clientAge)){
+            Alert al = new Alert(Alert.AlertType.INFORMATION);
+            al.setContentText("Мноо си малък пич!");
+            al.show();
+            return;
+        }
+        Client customer = new Client(
+                firstName.getText(),
+                lastName.getText(),
+                clientAge,
+                email.getText(),
+                phone.getText()
+        );
+        
+        clientsTable.getItems().add(customer);
+    }
+
+    @FXML
+    private void handleClearForm(ActionEvent event) {
+    }
+    
+    private Boolean validateAge(int age){
+        return age > 16;
+    }
 
 }
